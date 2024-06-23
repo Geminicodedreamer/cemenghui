9173940e606d8806121d2359d81f203c46aca8b6
yiqiufan <3581385570@qq.com>
2024-06-23 14:36:11 +0800

    hdx分支合并

---
+++ frontend/src/views/Meeting/AddMeeting.vue
@@ -0,0 +1,16 @@
+<template>
+    <ContentField>  </ContentField>
+</template>
+
+<script>
+import ContentField from '../../components/ContentField.vue'
+
+export default {
+    components: {
+        ContentField,
+    }
+}
+</script>
+
+<style scoped>
+</style>
